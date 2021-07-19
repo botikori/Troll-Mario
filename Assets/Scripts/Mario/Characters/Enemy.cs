f1d3e7d900ab16534294c860b46dad1986b3c5e8
botikori <botikori@pop-os.localdomain>
2021-07-19 21:35:41 +0200

    enemy edge detection, turn around

--- Assets/Scripts/Mario/Characters/Enemy.cs
+++ Assets/Scripts/Mario/Characters/Enemy.cs
@@ -4,16 +4,23 @@ namespace Mario
 {
     public abstract class Enemy : Character
     {
-        [SerializeField] private Transform leftSideRaycast;
-        [SerializeField] private Transform rightSideRaycast;
-
+        [SerializeField] private Transform rightPos;
+        [SerializeField] private Transform leftPos;
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        
         private Vector2 _direction = Vector2.left;
+
+        public Vector2 Direction
+        {
+            get => _direction;
+            set => _direction = value;
+        }
 
         protected override void Update()
         {
-            //base.Update();
-
-            TurnAround();
+            base.Update();
+            
+            DetectEdges();
         }
 
         protected override void Move()
@@ -25,9 +32,25 @@ namespace Mario
         {
         }
 
-        private void TurnAround()
+        private void DetectEdges()
         {
+            Vector2 rayRightStartPos = new Vector2(rightPos.position.x, rightPos.position.y);
+            Vector2 rayLeftStartPos = new Vector2(leftPos.position.x, leftPos.position.y);
             
+            RaycastHit2D rightHit = Physics2D.Raycast(rayRightStartPos, Vector2.down, 0.1f);
+            RaycastHit2D leftHit = Physics2D.Raycast(rayLeftStartPos, Vector2.down, 0.1f);
+            
+            if (rightHit.collider == null && GetIsGrounded())
+            {
+                _direction = Vector2.right;
+                spriteRenderer.flipX = true;
+            }
+            
+            if (leftHit.collider == null && GetIsGrounded())
+            {
+                _direction = Vector2.left;
+                spriteRenderer.flipX = false;
+            }
         }
     }
 }
--- Assets/Scripts/Mario/Tiles/KillerBlock.cs
+++ Assets/Scripts/Mario/Tiles/KillerBlock.cs
@@ -6,8 +6,9 @@ namespace Mario
     {
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.gameObject.CompareTag("Player")) ;
+            if (other.gameObject.CompareTag("Player"))
             {
+                Debug.Log(other.gameObject.tag);
                 Player player = FindObjectOfType<Player>();
 
                 if (player != null)
